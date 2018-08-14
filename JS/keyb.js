00344dd6bde371aa19726daeba1b89b01e2ae1e4
Alexandr <alexandrmail32@gmail.com>
2018-08-14 13:01:53 +0300

    clear all unused styles and classes

--- JS/keyb.js
+++ JS/keyb.js
@@ -3,6 +3,8 @@ var keyb = document.querySelector('#keyboard');
 
 pushKeyStyle(keyb);
 changeLenguage(keyb);
+capsLock (keyb,'caps','case','shift');
+capsLock (keyb,'shift-button','lang','shift');
 		
 function pushKeyStyle(parent){
 	parent.addEventListener('mousedown',function func() {
@@ -17,17 +19,28 @@ function pushKeyStyle(parent){
 			return;
 		}
 		
-		if(!target.classList.contains('caps')){
+		if(!target.classList.contains('caps')&&!target.classList.contains('shift-button')){
 			target.classList.add('button-pushed');
 			target.addEventListener('mouseleave', function func() {
 			this.classList.remove('button-pushed');
 			this.removeEventListener('mouseleave',func);
 			});
+		}else if(target.classList.contains('shift-button')){
+			var elem = this.getElementsByClassName('shift-button');
+			for (var i = 0; i < elem.length; i++) {
+				elem[i].classList.toggle('button-pushed');
+			}
 		}else{
 			target.classList.toggle('button-pushed');
 		}
-		});
 
+		if (target.classList.contains('audio')) {
+			var image = target.getElementsByTagName('img');
+			for (var i = 0; i < image.length; i++) {
+				image[i].classList.toggle('eng');
+			}
+		}
+		});
 
 	parent.addEventListener('mouseup',function () {
 		var target;
@@ -36,7 +49,8 @@ function pushKeyStyle(parent){
 		}else if(event.target.closest('div').classList.contains('button')){
 			target = event.target.closest('div');
 		}
-		if(!target.classList.contains('caps')){
+
+		if(!target.classList.contains('caps')&&!target.classList.contains('shift-button')){
 		target.classList.remove('button-pushed');
 		}
 	});
@@ -57,5 +71,20 @@ function changeKeySymbols(key) {
 	for (var i = 0; i < symb.length; i++) {
 		symb[i].classList.toggle('eng');
 		symb[i].classList.toggle('ru');
-		}
 	}
+}
+
+function capsLock (parent,classEvent,classLetters,classShift) {
+	var elem = parent.getElementsByClassName(classEvent);
+	for (var i = 0; i < elem.length; i++) {
+		elem[i].addEventListener('mousedown', function () {
+
+		var letters = parent.getElementsByClassName(classLetters);
+			for (var i = 0; i < letters.length; i++) {
+				if(!letters[i].parentNode.hasAttribute('id')){
+					letters[i].classList.toggle(classShift);
+				}
+			}
+		});
+	}
+}
